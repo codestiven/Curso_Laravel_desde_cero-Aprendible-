ef712d9817aa2a60491545d3ece05a31119fd8b8
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-06 20:01:43 -0400

    06 - Rutas en Laravel 10 | #Laravel desde cero

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,29 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view( '/', 'welcome') ->name('welcome'); ;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/Chirps', function () {
+//     return "coa coal";
+// });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/Chirps/{chirp?}', function ($chirp = null) {
+    if($chirp == "coca cola") {
+        return to_route('Chirps');
+    }
 
-require __DIR__.'/auth.php';
+    return "coa coal" . $chirp;
+}) ->name('Chirps');
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
