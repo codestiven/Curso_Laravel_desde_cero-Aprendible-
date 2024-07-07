a5e013c94561d0a89173d9410075cd74aca0e2a3
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-06 22:42:29 -0400

    08 - Layout y navegación | #Laravel desde cero

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                     <x-nav-link :href="route('Chirps')" :active="request()->routeIs('Chirps')">
+                        COCA COLA
+                    </x-nav-link>
                 </div>
             </div>
 
